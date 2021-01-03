d4a65865ca398f9442cd4a4ad1ab973d24d2364b
ngurahbas <>
2021-01-03 17:29:54 +0800

    circle moving inside a circle

--- spiro.js
+++ spiro.js
@@ -17,18 +17,29 @@ const setData = () => {
         }
         userInput[prop] = document.forms.userInput[prop].value;
     }
-    console.log(userInput);
 }
 
 const maxR = 200;
 const midX = 250;
 const midY = 250;
-const rpm = 0.2;
+const rps = 0.1;
 
 var ctx;
 var canvas;
 
-clearCanvas = () => {
+const sinByRev = (rev) => {
+    let calRev = rev % 1;
+    let angle = calRev * Math.PI * 2;
+    return Math.sin(angle);
+}
+
+const cosByRev = (rev) => {
+    let calRev = rev % 1;
+    let angle = calRev * Math.PI * 2;
+    return Math.cos(angle);
+}
+
+const clearCanvas = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
@@ -37,18 +48,37 @@ const drawCircle = (ctx, x, y, r) => {
 }
 
 var start;
+var outRev = 0;
+var inRev = 0;
 const paint = (timeStamp) => {
     if (!start) {
         start = timeStamp;
-        console.log(timeStamp);
     }
+    
+    let outR = maxR;
 
-    let outR = maxR;    
+    let elTime = timeStamp - start;
+    let roundEltime = Math.round(elTime);
+    let inRev = rps * roundEltime / 1000;
+    let outRev = inRev * (userInput.inR / userInput.outR);
+  
+    let radiusRatio = userInput.inR / userInput.outR;
+    let inR = maxR * radiusRatio;
+    let cToC = outR - inR;
+    let inX = midX + cToC * sinByRev(outRev);
+    let inY = midY - cToC * cosByRev(outRev);
 
-    ctx.beginPath();
+    console.log(maxR * radiusRatio);
+    console.log("x,y = ", inX, inY);
+
     clearCanvas();
+    ctx.beginPath(); 
     drawCircle(ctx, midX, midY, outR);
     ctx.stroke();
+    ctx.beginPath(); 
+    drawCircle(ctx, inX, inY, inR);
+    ctx.stroke();
+
 };
 
 var intPaint = setInterval(() => {window.requestAnimationFrame(paint);}, 1000);
