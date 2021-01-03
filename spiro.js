34716030c61e52451619286ecb95aa7f7e292f28
ngurahbas <>
2021-01-03 13:31:51 +0800

    add data to ui sync

--- spiro.js
+++ spiro.js
@@ -1,1 +1,15 @@
-console.log("loaded properly");
+var userInput = {
+    inR: 1,
+    outR: 2,
+    mPos: 0.5,
+};
+
+const setForm = () => {
+    for (let prop in userInput) {
+        document.forms.userInput[prop].value = userInput[prop];
+    }
+};
+
+window.onload = () => {
+    setForm();
+};
