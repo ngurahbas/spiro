844182a092005fc9ba0784de0a1eafe3ba77ae2d
ngurahbas <>
2021-03-28 19:12:17 +0800

    restructuring

--- src/index.js
+++ src/index.js
@@ -12,51 +12,49 @@ const midX = 250;
 const midY = 250;
 
 var calc_revToDot = new Map();
-var calc_lastCalcInRev = 0;
-var calc_lastCalcOutRev = 0;
-
-const calculateDots = () => {
-    const calc_revPerCycle = 1;
-    const calc_revInc = 0.01;
-
-    let inRev,outRev;
-    let calcFinished = false;
-    for (inRev = calc_lastCalcInRev; inRev <= (calc_lastCalcInRev + calc_revPerCycle); inRev += calc_revInc) {
-        inRev = Math.round(inRev*100)/100;
-        outRev = inRev * (userInput.inR / userInput.outR);
-
-        let radiusRatio = userInput.inR / userInput.outR;
-        let inR = maxR * radiusRatio;
-        let cToC = maxR - inR;
-        let inX = midX + cToC * sinByRev(-outRev);
-        let inY = midY - cToC * cosByRev(-outRev);
-
-        let mRadius = inR * userInput.mPos;
-        let markerX = inX + mRadius * sinByRev(inRev);
-        let markerY = inY - mRadius * cosByRev(inRev);
-
-        calc_revToDot.set(inRev, {
-            x: markerX,
-            y: markerY 
-        });
-        if (inRev > 0 && inRev % 1 == 0 && outRev % 1 == 0) {
-            calcFinished = true;
-            break;
-        }
-
-    }
-    calc_lastCalcInRev = inRev;
-    calc_lastCalcOutRev = outRev;
-    if (!calcFinished) {
-        setTimeout(calculateDots, 5);
-    }
-};
 
 const startCalculateDots = () => {
     calc_revToDot = new Map();
-    calc_lastCalcInRev = 0;
-    calc_lastCalcOutRev = 0;
-    calculateDots();
+    let calc_lastCalcInRev = 0;
+    let calc_lastCalcOutRev = 0;
+    const calculateDots = () => {
+        const calc_revPerCycle = 1;
+        const calc_revInc = 0.01;
+    
+        let inRev,outRev;
+        let calcFinished = false;
+        for (inRev = calc_lastCalcInRev; inRev <= (calc_lastCalcInRev + calc_revPerCycle); inRev += calc_revInc) {
+            inRev = Math.round(inRev*100)/100;
+            outRev = inRev * (userInput.inR / userInput.outR);
+    
+            let radiusRatio = userInput.inR / userInput.outR;
+            let inR = maxR * radiusRatio;
+            let cToC = maxR - inR;
+            let inX = midX + cToC * sinByRev(-outRev);
+            let inY = midY - cToC * cosByRev(-outRev);
+    
+            let mRadius = inR * userInput.mPos;
+            let markerX = inX + mRadius * sinByRev(inRev);
+            let markerY = inY - mRadius * cosByRev(inRev);
+    
+            calc_revToDot.set(inRev, {
+                x: markerX,
+                y: markerY 
+            });
+            if (inRev > 0 && inRev % 1 == 0 && outRev % 1 == 0) {
+                calcFinished = true;
+                break;
+            }
+    
+        }
+        calc_lastCalcInRev = inRev;
+        calc_lastCalcOutRev = outRev;
+        if (!calcFinished) {
+            setTimeout(calculateDots, 5);
+        }
+    };
+    
+    return calculateDots();
 };
 
 const clearCanvas = (context) => {
