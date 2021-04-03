0bb0e4f98060553f46ec5e97bb33aa8fdcc6c71f
ngurahbas <>
2021-04-03 14:25:30 +0800

    Restructing index.js, moving draw dots to canvas.js

--- src/canvas.js
+++ src/canvas.js
@@ -8,4 +8,25 @@ const drawCircle = (context, x, y, r) => {
     context.stroke();
 }
 
-export {clearCanvas, drawCircle};
+const drawDots = (context, revToDots, lastRev) => {
+    let keyIt = revToDots.keys();
+    let k1 = keyIt.next().value;
+
+    context.beginPath();
+    let result = keyIt.next();
+    while (!result.done && result.value < lastRev) {
+        let pos1 = revToDots.get(k1);
+        let k2 = result.value;
+        let pos2 = revToDots.get(k2);
+
+        context.moveTo(pos1.x, pos1.y);
+        context.lineTo(pos2.x, pos2.y);
+
+        k1 = k2;
+        result = keyIt.next();
+    }
+
+    context.stroke();
+}
+
+export {clearCanvas, drawCircle, drawDots};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import {sinByRev, cosByRev} from './math.js'
-import {setForm, setData} from './binding.js'
-import {clearCanvas, drawCircle} from './canvas.js'
+import { sinByRev, cosByRev } from './math.js'
+import { setForm, setData } from './binding.js'
+import { clearCanvas, drawCircle, drawDots } from './canvas.js'
 
 var userInput = {
     inR: 9,
@@ -12,41 +12,39 @@ const maxR = 200;
 const midX = 250;
 const midY = 250;
 
-var calc_revToDot = new Map();
-
 const startCalculateDots = () => {
-    calc_revToDot = new Map();
+    let calc_revToDot = new Map();
     let calc_lastCalcInRev = 0;
     let calc_lastCalcOutRev = 0;
     const calculateDots = () => {
         const calc_revPerCycle = 1;
         const calc_revInc = 0.01;
-    
-        let inRev,outRev;
+
+        let inRev, outRev;
         let calcFinished = false;
         for (inRev = calc_lastCalcInRev; inRev <= (calc_lastCalcInRev + calc_revPerCycle); inRev += calc_revInc) {
-            inRev = Math.round(inRev*100)/100;
+            inRev = Math.round(inRev * 100) / 100;
             outRev = inRev * (userInput.inR / userInput.outR);
-    
+
             let radiusRatio = userInput.inR / userInput.outR;
             let inR = maxR * radiusRatio;
             let cToC = maxR - inR;
             let inX = midX + cToC * sinByRev(-outRev);
             let inY = midY - cToC * cosByRev(-outRev);
-    
+
             let mRadius = inR * userInput.mPos;
             let markerX = inX + mRadius * sinByRev(inRev);
             let markerY = inY - mRadius * cosByRev(inRev);
-    
+
             calc_revToDot.set(inRev, {
                 x: markerX,
-                y: markerY 
+                y: markerY
             });
             if (inRev > 0 && inRev % 1 == 0 && outRev % 1 == 0) {
                 calcFinished = true;
                 break;
             }
-    
+
         }
         calc_lastCalcInRev = inRev;
         calc_lastCalcOutRev = outRev;
@@ -54,30 +52,10 @@ const startCalculateDots = () => {
             setTimeout(calculateDots, 5);
         }
     };
-    
-    return calculateDots();
+
+    calculateDots();
+    return calc_revToDot;
 };
-
-const drawDots = (context, lastRev) => {
-    let keyIt= calc_revToDot.keys();
-    let k1 = keyIt.next().value;
-
-    context.beginPath();
-    let result = keyIt.next();
-    while (!result.done && result.value < lastRev) {
-        let pos1 = calc_revToDot.get(k1);
-        let k2 = result.value;
-        let pos2 = calc_revToDot.get(k2);
-
-        context.moveTo(pos1.x, pos1.y);
-        context.lineTo(pos2.x, pos2.y);
-
-        k1 = k2;
-        result = keyIt.next();
-    }
-
-    context.stroke();
-}
 
 const rps = 1;
 
@@ -85,52 +63,56 @@ var ctx;
 var canvas;
 
 var start;
-const paint = (timeStamp) => {
-    if (!start) {
-        start = timeStamp;
-    }
-
-    let outR = maxR;
-
-    let roundEltime = Math.round(timeStamp - start);
-    let inRev = rps * roundEltime / 1000;
-    let outRev = inRev * (userInput.inR / userInput.outR);
-
-    let radiusRatio = userInput.inR / userInput.outR;
-    let inR = maxR * radiusRatio;
-    let cToC = outR - inR;
-    let inX = midX + cToC * sinByRev(-outRev);
-    let inY = midY - cToC * cosByRev(-outRev);
-
-    let mRadius = inR * userInput.mPos;
-    let markerX = inX + mRadius * sinByRev(inRev);
-    let markerY = inY - mRadius * cosByRev(inRev);
-
-    clearCanvas(ctx, canvas.width, canvas.height);
-    drawDots(ctx, inRev); 
-    drawCircle(ctx, midX, midY, outR);
-    drawCircle(ctx, inX, inY, inR);
-    drawCircle(ctx, markerX, markerY, 4);
-};
-
 var intPaint;
-const startOver = () => {
+const startOver = (revToDots) => {
+    const paint = (timeStamp) => {
+        if (!start) {
+            start = timeStamp;
+        }
+
+        let outR = maxR;
+
+        let roundEltime = Math.round(timeStamp - start);
+        let inRev = rps * roundEltime / 1000;
+        let outRev = inRev * (userInput.inR / userInput.outR);
+
+        let radiusRatio = userInput.inR / userInput.outR;
+        let inR = maxR * radiusRatio;
+        let cToC = outR - inR;
+        let inX = midX + cToC * sinByRev(-outRev);
+        let inY = midY - cToC * cosByRev(-outRev);
+
+        let mRadius = inR * userInput.mPos;
+        let markerX = inX + mRadius * sinByRev(inRev);
+        let markerY = inY - mRadius * cosByRev(inRev);
+
+        clearCanvas(ctx, canvas.width, canvas.height);
+        drawDots(ctx, revToDots, inRev);
+        drawCircle(ctx, midX, midY, outR);
+        drawCircle(ctx, inX, inY, inR);
+        drawCircle(ctx, markerX, markerY, 4);
+    };
+
     start = null;
-    intPaint = setInterval(() => { window.requestAnimationFrame(paint); }, 20);
+    intPaint = setInterval(() => {
+        window.requestAnimationFrame(paint);
+    }, 20);
 };
 
 window.onload = () => {
     document.forms.userInput.oninput = () => {
         setData(document.forms.userInput, userInput);
-        
+
         clearInterval(intPaint);
-        startOver();
-        startCalculateDots();
+
+        let revToDots = startCalculateDots();
+        startOver(revToDots);
     }
     setForm(document.forms.userInput, userInput);
 
     canvas = document.getElementById("drawingCanvas");
     ctx = canvas.getContext("2d");
-    startOver();
-    startCalculateDots();
+
+    let revToDots = startCalculateDots();
+    startOver(revToDots);
 };
