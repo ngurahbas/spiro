b094cb1402a4bf65d1849a7a5a1e5a456f409340
ngurahbas <>
2021-06-05 19:45:35 +0800

    move codes

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,13 +1,13 @@
+export interface CanvasModel {
+    width: number;
+    height: number;
+    context: RenderingContext;
+}
+
 export function getCanvasModel(canvasElement: HTMLCanvasElement): CanvasModel {
     return {
         width: canvasElement.width,
         height: canvasElement.height,
         context: canvasElement.getContext("2d")
     };
-}
-
-export interface CanvasModel {
-    width: number;
-    height: number;
-    context: RenderingContext;
 }
