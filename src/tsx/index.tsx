15599d329619f5ec313946038b8e92164444f65c
ngurahbas <>
2021-07-07 21:14:23 +0800

    remove uneeded code

--- src/tsx/index.tsx
+++ src/tsx/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from "react-dom";
 
 import { MainForm } from "./app";
 
-let R: number = 4;
 ReactDOM.render(
     <MainForm staticCircleRadius={4} />,
     document.getElementById("form-wrapper")
