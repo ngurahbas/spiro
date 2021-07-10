ff4055a62852ac31a6a9472b76dd911248456df9
ngurahbas <>
2021-07-10 13:51:32 +0800

    adding tailwindcss nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-const tailwindcss = require('tailwindcss');
 module.exports = {
     plugins: [
-        tailwindcss
+        require('postcss-import'),
+        require('tailwindcss/nesting'),
+        require('tailwindcss')
     ],
 };
