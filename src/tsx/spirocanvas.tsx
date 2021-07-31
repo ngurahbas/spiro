6035351b0b3f006c96d0b042c94fec130ae2e1d8
ngurahbas <>
2021-07-31 19:30:56 +0800

    adding wiring canvas controller to jsx

--- src/tsx/spirocanvas.tsx
+++ src/tsx/spirocanvas.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { CanvasController } from "../ts/canvas";
 
 interface SpiroCanvasProps {
     staticR: number;
@@ -8,15 +9,22 @@ interface SpiroCanvasProps {
 }
 
 export class SpiroCanvas extends React.Component<SpiroCanvasProps> {
-    canvas: React.Ref<HTMLCanvasElement>;
+    canvasRef: React.RefObject<HTMLCanvasElement>;
+    canvasController: CanvasController;
     
     constructor(props: SpiroCanvasProps) {
         super(props);
+        this.canvasRef = React.createRef();
+        this.canvasRef.current
+    }
+
+    componentDidMount() {
+        this.canvasController = new CanvasController(this.canvasRef.current);
     }
 
     render() {
         return (
-            <canvas width={this.props.canvasWidth} height={this.props.canvasWidth} ref={this.canvas}></canvas>
+            <canvas width={this.props.canvasWidth} height={this.props.canvasWidth} ref={this.canvasRef}></canvas>
         );
     }
 }
