3682f8aca030c5cec179d51080b702c8e9883a91
ngurahbas <>
2021-07-31 20:00:10 +0800

    adding math

--- src/ts/math.ts
+++ src/ts/math.ts
@@ -1,9 +1,27 @@
-export function sinByRev(rev: number):number {
+export function sinByRev(rev: number): number {
     let angle = rev * Math.PI * 2;
     return Math.sin(angle);
 }
 
-export function cosByRev(rev: number):number {
+export function cosByRev(rev: number): number {
     let angle = rev * Math.PI * 2;
     return Math.cos(angle);
+}
+
+export function gcd(a: number, b: number) {
+    if (a == 0)
+        return b;
+
+    while (b != 0) {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+
+    return a;
+}
+
+export function lcm(a: number, b: number) {
+    return (a * b) / gcd(a, b);
 }
